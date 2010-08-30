293a34153f0b04fda43528a53bc1e7d04ff01e2a
haineault@gmail.com <haineault@gmail.com@cbbb72fd-229f-76fc-86c8-4767d43fc7c5>
2010-08-30 03:53:14 +0000

    fixed decimal to hex conversion bug (http://stackoverflow.com/questions/3597610/hex-rgb-hsv-color-space-conversion-with-python)

--- trunk/colors/urls.py
+++ trunk/colors/urls.py
@@ -9,13 +9,20 @@ urlpatterns = patterns('',
             'colors_tests': [
                 '000000', # Black
                 'ff0000', # Red
-                '00ff00', # Green
+                '008000', # Green 
+                '008080', # teal
+                '808000', # Olive
+                '00ff00', # Lime
                 '0000ff', # Blue
                 'ffff00', # Yellow
-                '00ffff', # Cyan
-                'ff00ff', # Magenta
-                'c0c0c0', # Grey
+                '00ffff', # Aqua / cyan
+                'ff00ff', # Fuchsia / magenta
+                '808080', # Gray
                 'ffffff', # White
+                '000080', # Navy
+                'C0C0C0', # Silver
+                '800080', # Purple
+                '800000', # Maroon
             ],
             'lightness_range':  range(0, 101, 10),
             'saturation_range': range(0, 101, 10),
