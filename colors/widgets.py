e445e23596c1af993f1b2b8bbfa9b4194e91af1d
haineault <haineault@gmail.com>
2010-12-12 03:35:56 +0000

    small fixes

--- colors/widgets.py
+++ colors/widgets.py
@@ -12,7 +12,7 @@ class ColorPickerWidget(forms.TextInput):
             )
         }
         js = (
-            settings.MEDIA_URL + 'colors/colorpicker.js',
+            settings.MEDIA_URL + 'colors/js/colorpicker.js',
         )
 
     def __init__(self, language=None, attrs=None):
@@ -21,14 +21,12 @@ class ColorPickerWidget(forms.TextInput):
 
     def render(self, name, value, attrs=None):
         rendered = super(ColorPickerWidget, self).render(name, value, attrs)
-        #var preview = $('<div id="customWidget"><div id='colorSelector2'><div style='background-color: #00ff00'></div></div>').insertAfter('#id_%s');
         return rendered + mark_safe(u"""<script type="text/javascript">(function($){
 $(function(){
     var preview = $('<div class="color-picker-preview"><div style="background-color:#%s"></div></div>').insertAfter('#id_%s');
     $('#id_%s').ColorPicker({
         color: '%s',
         onSubmit: function(hsb, hex, rgb, el) { $(el).val(hex); $(el).ColorPickerHide();$(preview).find('div').css('backgroundColor', '#' + hex); },
-        onBeforeShow: function () { $(this).ColorPickerSetColor(this.value); },
-        //onChange: function (hsb, hex, rgb) { $(preview).find('div').css('backgroundColor', '#' + hex); }
+        onBeforeShow: function () { $(this).ColorPickerSetColor(this.value); }
     }).bind('keyup', function(){ $(this).ColorPickerSetColor(this.value); });
-});})(typeof(django) != 'undefined' && django.jQuery || jQuery);</script>""" % (value, name, name, value))
+});})(django.jQuery);</script>""" % (value, name, name, value))
